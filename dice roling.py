b99c7f00cb75278b14f5ffafe892484850f89dbb
Pavankalyan442 <pavankalyan.r2003@gmail.com>
2025-06-17 12:14:24 +0530

    Add files via upload

---
+++ dice roling.py
@@ -0,0 +1,12 @@
+import random
+while True:
+    choice=input("Roll the dice (y/n):")
+    if choice=="y" or choice== "Y":
+        die1=random.randint(1,6)
+        die2=random.randint(1,6)
+        print(f"({die1},{die2})")
+    elif choice=="n" or choice=="N":
+        print("thank you for playing")
+        break
+    else:
+        print("choice is invalid")
